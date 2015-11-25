8b53686d438b7b1bb60192a7aaf56d71dcdc8d7f
Anelia Stavreva <nicki.ts@gmail.com>
2015-11-26 01:06:11 +0200

    add socket service

--- WebContent/fancychat/public/js/controllers.js
+++ WebContent/fancychat/public/js/controllers.js
@@ -4,8 +4,22 @@ appControllers.controller("mainCtrl", function($scope){
   $scope.message = "Test";
 });
 
-appControllers.controller("ChatCtrl", function($scope){
-  $scope.message = "Test";
+appControllers.controller("ChatCtrl", function($scope, socketService){
+  $scope.message = {};
+  $scope.messages = [];
+
+  socketService.init("1", "me");
+
+  socketService.onReceive(function(data){
+    if(data){
+      $scope.messages.push(data);
+    }
+  });
+
+  $scope.sendMessage = function(text){
+    socketService.send(text);
+    $scope.message.text = "";
+  }
 });
 
 appControllers.controller("CallCtrl", function($scope){
--- WebContent/fancychat/public/js/services.js
+++ WebContent/fancychat/public/js/services.js
@@ -2,39 +2,52 @@ var appServices = angular.module("appServices", []);
 
 appServices.factory("restService", function($http){
   return {
-    get: function(url){
+    get: function(url, successFn, errorFn){
       $http({
         method: 'GET',
         url: url
       }).then(function successCallback(response) {
-          // this callback will be called asynchronously
-          // when the response is available
+          if(successFn) { successFn(response); }
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          if(errorFn) { errorFn(response); }
         });
     },
-    get: function(url){
+    post: function(url, data){
       $http({
         method: 'POST',
         url: url,
         headers: {
          'Content-Type': "application/json"
         },
-        data: { test: 'test' }
+        data: data
       }).then(function successCallback(response) {
-          // this callback will be called asynchronously
-          // when the response is available
+          if(successFn) { successFn(response); }
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          if(errorFn) { errorFn(response); }
         });
     }
   }
 });
 
 appServices.factory("socketService", function(){
+  var socket = io();
   return {
-    
+    init: function(room, username){
+      socket
+      //join the room
+      socket.emit('room', room);
+      // Tell the server your username
+      socket.emit('add user', username);
+    },
+    send: function(message, room){
+      // tell server to execute 'new message' and send along one parameter
+      socket.emit('new message', message);
+    },
+    onReceive: function(receiveFn){
+      // Whenever the server emits 'new message', update the chat body
+      socket.on('new message', function (data) {
+        if(receiveFn){ receiveFn(data); }
+      });
+    }
   }
 });
